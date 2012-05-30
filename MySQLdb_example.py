003beb19e7521094d1698a9bd0710454cd424b8b
Denis Stepulenok <super.denis@gmail.com>
2012-05-30 16:32:45 +0300

    MySQLdb

---
+++ MySQLdb_example.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import MySQLdb
+db=MySQLdb.connect(host="openserver",user="root",passwd="",db="stden",use_unicode=True)
+db.set_character_set('utf8')
+c=db.cursor()
+c.execute('SET NAMES utf8;')
+c.execute('SET CHARACTER SET utf8;')
+c.execute('SET character_set_connection=utf8;')
+
+# Устанавливаем UTF-8 для вывода в консоли
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# Перенаправляем вывод в файл
+sys.stdout = open('MySQLdb_example.log', 'w')
+
+# Создаём запрос и выполняем его 
+sql = "SELECT * FROM user"
+c.execute(sql)
+
+# Fetch all results from the cursor into a sequence and close the connection
+print u"Запрос: "+sql # Все строки надо делать в Unicode, хех :)
+res = c.fetchall()
+for row in res:
+  print u"-- Новая строка --"
+  for f in row:
+    print f
+db.close()
